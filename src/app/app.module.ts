b0bc55758fa50c30d6d5c9b25b23c160727705d3
alaeddine <alaeddinehamroun@insat.u-carthage.tn>
2021-07-20 08:41:43 +0100

    first commit: setting the events list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { navBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    navBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,21 @@
+import { Component,Input, Output,EventEmitter } from "@angular/core";
+
+
+@Component({
+    selector: 'event-thumbnail',
+    templateUrl: './event-thumbnail.component.html',
+    styles: [`
+        .thumbnail {min-height: 210px; }
+        .pad-left {margin-left: 10px; }
+        .well div {color: #bbb; }
+        `]
+})
+export class EventThumbnailComponent {
+    @Input() event: any;
+    getStartTimeStyle(): any{
+        if(this.event && this.event.time === '8:00 am')
+            return {color: '#003300', 'font-weight': 'bold'};
+        return {}
+    }
+}
+
